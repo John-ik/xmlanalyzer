18e3d069583391848410baa2d52fecdb879bc1f4
johnik <kotovjoni@gmail.com>
2024-06-15 22:05:01 +0300

    Научился брать ВСЕ валидные файлы из указанных путей (в т.ч. из подпапок); Соединять их в один общий документ. Возможно внедрение проверок для любой части xml - общего документа.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,88 @@
-import std.stdio;
+import std.logger;
 
-void main()
+import std.stdio;
+import std.file;
+import std.algorithm : map, joiner, filter;
+
+import std.array : array, Appender, appender;
+import std.range : walkLength;
+import std.typecons : Flag, Yes, No;
+import std.exception : ifThrown;
+
+import dxml.parser;
+import dxml.writer;
+
+alias AddNewRoot = Flag!"AddNewRoot";
+
+string writeXmlFromEntitis (IR)(IR xmlEntities, AddNewRoot addNewRoot = AddNewRoot.no)
 {
-	writeln("Edit source/app.d to start your project.");
+	auto writer = xmlWriter(appender!string());
+
+	if (addNewRoot)
+		writer.writeStartTag("newrooooot");
+
+	foreach (entity; xmlEntities)
+	{
+		final switch (entity.type())
+		{
+		case EntityType.comment:
+			writer.writeComment(entity.text());
+			break;
+		case EntityType.cdata:
+			writer.writeCDATA(entity.text());
+			break;
+		case EntityType.elementEmpty:
+			writer.writeStartTag(entity.name(), EmptyTag.yes);
+			break;
+		case EntityType.elementEnd:
+			writer.writeEndTag(entity.name());
+			break;
+		case EntityType.elementStart:
+			writer.openStartTag(entity.name());
+			foreach (attr; entity.attributes())
+				writer.writeAttr(attr.name, attr.value);
+			writer.closeStartTag();
+			break;
+		case EntityType.pi:
+			writer.writePI(entity.name(), entity.text());
+			break;
+		case EntityType.text:
+			writer.writeText(entity.text());
+			break;
+
+		}
+	}
+
+	if (addNewRoot)
+		writer.writeEndTag("newrooooot");
+	
+	return writer.output().data();
+}
+
+
+void main(string[] args)
+{
+	infof("Args: %s", args);
+	
+	string[] xmlFiles;
+	foreach (string path; args)
+	{
+		if (isFile(path)) xmlFiles ~= path;
+		if (isDir(path))
+			foreach(file; dirEntries(path, SpanMode.depth))
+			{
+				xmlFiles ~= file;
+			}
+		
+	}
+
+
+	auto xmlDocs = map!(a => readText(a).parseXML!simpleXML().ifThrown(EntityRange!(simpleXML, string)()))(xmlFiles[1..$])
+					.filter!(a => a != EntityRange!(simpleXML, string)())
+					.joiner();
+	
+
+	auto output = writeXmlFromEntitis(xmlDocs, AddNewRoot.yes);
+
+	writeln(output);
 }
