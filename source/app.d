2c70761067793468ca69c397d972966fdda48ade
johnik <kotovjoni@gmail.com>
2024-06-17 13:51:24 +0300

    Переделал всё на DOM. Оно изменчиво, можно спокойно вставлять в любую точку (почти). Сдалал DOM->string для записи.

--- source/app.d
+++ source/app.d
@@ -2,17 +2,32 @@ import std.logger;
 
 import std.stdio;
 import std.file;
-import std.algorithm : map, joiner, filter;
+import std.algorithm : map, joiner, filter, copy;
 
 import std.array : array, Appender, appender;
-import std.range : walkLength;
+import std.range : walkLength, take, ElementType;
 import std.typecons : Flag, Yes, No;
-import std.exception : ifThrown;
+import std.exception : ifThrown, enforce;
+import std.meta : AliasSeq;
 
+import dxml.dom;
 import dxml.parser;
 import dxml.writer;
+import std.path;
 
 alias AddNewRoot = Flag!"AddNewRoot";
+
+alias MyEntityTemplate = AliasSeq!(simpleXML, string);
+
+DOMEntity!R entityNone (R) ()
+{
+	return DOMEntity!(R)();
+}
+
+DOMEntity!string entityNone () ()
+{
+	return entityNone!(string)();
+}
 
 string writeXmlFromEntitis (IR)(IR xmlEntities, AddNewRoot addNewRoot = AddNewRoot.no)
 {
@@ -32,7 +47,10 @@ string writeXmlFromEntitis (IR)(IR xmlEntities, AddNewRoot addNewRoot = AddNewRo
 			writer.writeCDATA(entity.text());
 			break;
 		case EntityType.elementEmpty:
-			writer.writeStartTag(entity.name(), EmptyTag.yes);
+			writer.openStartTag(entity.name());
+			foreach (attr; entity.attributes())
+				writer.writeAttr(attr.name, attr.value);
+			writer.closeStartTag(EmptyTag.yes);
 			break;
 		case EntityType.elementEnd:
 			writer.writeEndTag(entity.name());
@@ -59,13 +77,64 @@ string writeXmlFromEntitis (IR)(IR xmlEntities, AddNewRoot addNewRoot = AddNewRo
 	return writer.output().data();
 }
 
+/// Прямой обход дерева
+string writeXmlFromDOM (IR)(IR xmlDom)
+{
+	auto writer = xmlWriter(appender!string());
+	
+	void writeNode(IR node)
+	{
+		// writeln(node);
+		final switch (node.type())
+		{
+		case EntityType.comment:
+			writer.writeComment(node.text());
+			break;
+		case EntityType.cdata:
+			writer.writeCDATA(node.text());
+			break;
+		case EntityType.elementEmpty:
+			writer.openStartTag(node.name());
+			foreach (attr; node.attributes())
+				writer.writeAttr(attr.name, attr.value);
+			writer.closeStartTag(EmptyTag.yes);
+			break;
+		case EntityType.elementEnd:
+			enforce("В DOM такой тип не встречается. Чтоб избежать UB - ошибка");
+			break;
+		case EntityType.elementStart:
+			if (node.name() != "") {
+				writer.openStartTag(node.name());
+				foreach (attr; node.attributes())
+					writer.writeAttr(attr.name, attr.value);
+				writer.closeStartTag();
+			}
+			foreach (child; node.children())
+				writeNode(child);
+			if (node.name() != "") 
+				writer.writeEndTag(node.name());
+			break;
+		case EntityType.pi:
+			writer.writePI(node.name(), node.text());
+			break;
+		case EntityType.text:
+			writer.writeText(node.text());
+			break;
+		}
+	}
+	
+	writeNode(xmlDom);
+
+	return writer.output().data();
+}
+
 
 void main(string[] args)
 {
 	infof("Args: %s", args);
 	
 	string[] xmlFiles;
-	foreach (string path; args)
+	foreach (string path; args[1..$])
 	{
 		if (isFile(path)) xmlFiles ~= path;
 		if (isDir(path))
@@ -76,13 +145,50 @@ void main(string[] args)
 		
 	}
 
+	info(xmlFiles);
 
-	auto xmlDocs = map!(a => readText(a).parseXML!simpleXML().ifThrown(EntityRange!(simpleXML, string)()))(xmlFiles[1..$])
-					.filter!(a => a != EntityRange!(simpleXML, string)())
-					.joiner();
+	DOMEntity!string[] xmlDocs;
+	foreach (path; xmlFiles)
+	{
+		auto a = readText(path).parseDOM().ifThrown(entityNone());
+		if (a == entityNone())
+			continue;
+		xmlDocs ~= a;
+	}
+
+	// writeln(xmlDocs);
+
+	string output = writeXmlFromDOM(xmlDocs[0]);
+	writeln(output);
+
+
+
+	// God-xml
 	
 
-	auto output = writeXmlFromEntitis(xmlDocs, AddNewRoot.yes);
+	// // God-xml
+	// EntityRange!(MyEntityTemplate) godXml;
+	// foreach (string path; xmlFiles)
+	// {
+	// 	auto a = readText(path).parseXML!simpleXML().ifThrown(entityNone());
+	// 	if (a == entityNone())
+	// 		continue;
+		
+	// }
+	// auto xmlDocs = xmlFiles
+	// 				.map!(a => readText(a).parseXML!simpleXML().ifThrown(entityNone()))
+	// 				.filter!(a => a != entityNone())
+	// 				.joiner();
+	// pragma(msg, ElementType!(typeof(xmlDocs)));
+	// // auto dom = parseDOM(xmlDocs);
 
-	writeln(output);
+	// foreach (xml; xmlDocs)
+	// {
+	// 	// Проверки
+	// }
+
+
+	// auto output = writeXmlFromEntitis(xmlDocs, AddNewRoot.yes);
+
+	// writeln(output);
 }
